fa017300d292ff45b5f6137cef7429709e7f10c4
crisalgarol <crisalgarol@gmail.com>
2018-10-24 22:10:20 -0500

    Primera version puramente visual sin logico

---
+++ miniobservium/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MiniobserviumConfig(AppConfig):
+    name = 'miniobservium'
---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+function activarModal(){
+		m = document.getElementById("customModal");
+		m.style.visibility = (m.style.visibility == "visible") ? "hidden" : "visible";
+}
