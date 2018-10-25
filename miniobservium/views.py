c906c8483b12c38bf4e5c585fbb2e3982a7cd473
crisalgarol <crisalgarol@gmail.com>
2018-10-24 23:18:02 -0500

    Se envian por el metodo post los elementos para agregar un nuevo agente

--- miniobservium/views.py
+++ miniobservium/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
+
+    #Declaramos un diccionario para poder recuperar los objetos recibidos
+    datosObjeto = {}
+
+    if(request.method == 'POST'):
+        datosObjeto['hostname'] = request.POST.get('hostnameInput', '')
+        datosObjeto['version'] = request.POST.get('versionsnmpInput', '')
+        datosObjeto['puerto'] = request.POST.get('puertoInput', '')
+        datosObjeto['comunidad'] = request.POST.get('comunidadInput', '')
+        datosObjeto['nombre'] = request.POST.get('nombreInput', '')
+
+        print(datosObjeto)
+
+
     return render(request, 'miniobservium/index.html',{})
 
 # Create your views here.
